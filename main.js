5fb8ac029e8ca94d70a105a1f37633f9e29bede1
Diegoelzd <diegoelizalde1999@hotmail.com>
2022-12-29 20:08:53 -0300

    Function that declares a winner done.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
-choices = ["Rock", "Paper", "Scissors"];
-const playerScore = 0;
-const computerScore = 0;
+CHOICES = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice(){
-const randomNumber = choices[Math.floor(Math.random() * choices.length)].toLowerCase(); 
+const randomNumber = CHOICES[Math.floor(Math.random() * CHOICES.length)].toLowerCase(); 
 return randomNumber;
 }
 
@@ -17,7 +17,12 @@ const userSelection = prompt("Rock, paper or scissor?").toLowerCase();
 playerSelection = userSelection;
 return playerSelection;
 
+
+
+
+
 }
+
 
 
 function playRound(){
@@ -27,20 +32,32 @@ console.log("Player:", playerSelection);
 console.log("Computer:",computerSelection);
 
 
-if(computerSelection == playerSelection){
+if(computerSelection === playerSelection){
     console.log("It's a tie!");
 
-} else if(computerSelection === "rock" && playerSelection === "paper" ||
- computerSelection == "paper" && playerSelection == "scissors"  ||
-  computerSelection == "scissors" &&playerSelection == "rock")
-  {console.log("You win!")} 
+    
+
+} else if((computerSelection === "rock" && playerSelection === "paper" )||
+( computerSelection == "paper" && playerSelection == "scissors" ) ||(
+  computerSelection == "scissors" && playerSelection == "rock"))
+  {console.log("You win!");
+  playerScore += 1;
+console.log("Player score:", playerScore);
+
+} 
 
 
 
 
-else if(computerSelection === "paper" && playerSelection === "rock" || 
-    computerSelection == "scissors" && playerSelection == "paper"  || computerSelection == "rock" &&
-    playerSelection == "scissors"){ console.log("You lose!" )}
+
+else if((computerSelection === "paper" && playerSelection === "rock") || 
+    (computerSelection == "scissors" && playerSelection == "paper")  || 
+    (computerSelection == "rock" && playerSelection == "scissors" ))
+    { console.log("You lose!" );
+    computerScore += 1;
+    console.log("Computer score:", computerScore)
+
+}
 
 
 }
@@ -61,4 +78,16 @@ for(i=0; i< 5; i++){
 game();
 
 
+function checkWinner(){
+if (playerScore > computerScore){
+console.log("Game it's over! The winner is player")
+} else if(computerScore > playerScore){
+    console.log("Game it's over! The winner is computer")
+} else if(playerScore == computerScore){
+console.log("Game it's over! It's a tie!")
+
+}
+
+}
+checkWinner();  
 
